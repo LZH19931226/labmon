1ccb803a67999673e549f8061783e1b4af8987e3
liuzhihao <1969994698@qq.com>
2021-03-31 17:18:26 +0800

    实现redis异步推送,保证推送完整,提供失败尝试机制

--- hc-message-ms/src/main/java/com/hc/my/message/HcMessageMsApplication.java
+++ hc-message-ms/src/main/java/com/hc/my/message/HcMessageMsApplication.java
@@ -3,9 +3,7 @@ package com.hc.my.message;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("com.hc.my.message.mapper")
 public class HcMessageMsApplication {
--- hc-message-ms/src/main/java/com/hc/my/message/notify/redis/RedisSynchronous.java
+++ hc-message-ms/src/main/java/com/hc/my/message/notify/redis/RedisSynchronous.java
@@ -1,7 +1,12 @@
 package com.hc.my.message.notify.redis;
 
+import com.hc.my.message.config.RedisTemplateUtil;
 import com.hc.my.message.model.Notification;
 import com.hc.my.message.notify.Notifier;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 /**
@@ -12,10 +17,24 @@ import org.springframework.stereotype.Service;
 @Service("PUSH")
 public class RedisSynchronous implements Notifier {
 
+    @Autowired
+    private RedisTemplateUtil redisTemplateUtil;
 
     @Override
     public String exec(Notification notification){
-        //return  WebSocketServer.sendInfo(notification.getMessageBodys(),notification.getPublishKey());
+        //规定redis缓存的对象json
+        String messageBodys = notification.getMessageBodys();
+        //规定redis缓存的key
+        String messageTitle = notification.getMessageTitle();
+        //如果是hash类型的redis 此处存储hashkey
+        String messageIntro = notification.getMessageIntro();
+        if (StringUtils.isNotEmpty(messageIntro)){
+            HashOperations<Object, Object, Object> objectObjectObjectHashOperations = redisTemplateUtil.opsForHash();
+            objectObjectObjectHashOperations.put(messageTitle,messageIntro,messageBodys);
+        }else {
+            ValueOperations<Object, Object> objectObjectValueOperations = redisTemplateUtil.opsForValue();
+            objectObjectValueOperations.set(messageTitle,messageBodys);
+        }
         return null;
     }
 }
