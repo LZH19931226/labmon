bff8ea659a2ee747ebeb00b807bad76a12a4b6b2
1969994698@qq.com <LZH3344520>
2022-03-09 10:48:44 +0800

    去掉跨域

--- HC-MON-ms/src/main/java/com/hc/config/WebAppConfigurer.java
+++ HC-MON-ms/src/main/java/com/hc/config/WebAppConfigurer.java
@@ -60,34 +60,5 @@ public class WebAppConfigurer extends WebMvcConfigurerAdapter {
         converters.add(fastConverter);
     }
 
-    /**
-     * 跨域配置
-     */
-    @Bean
-    public CorsFilter corsFilter()
-    {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        // 设置访问源地址
-        config.addAllowedOrigin("*");
-        // 设置访问源请求头
-        config.addAllowedHeader("*");
-        // 设置访问源请求方法
-        config.addAllowedMethod("*");
-        // 对接口配置跨域设置
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        super.addCorsMappings(registry);
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .allowedMethods("*")
-                .maxAge(3600);
-    }
 }
