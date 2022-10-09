38cde26ff48745f4473cb62ddb3c4de61b298cbf
chengkai <327909552@qq.com>
2022-10-09 15:59:56 +0800

    sys-添加confid为45的判断-ck

--- HC-MSCT-ms/src/main/java/com/hc/serviceimpl/WarningServiceImpl.java
+++ HC-MSCT-ms/src/main/java/com/hc/serviceimpl/WarningServiceImpl.java
@@ -37,7 +37,7 @@ public class WarningServiceImpl implements WarningService {
     @Autowired
     private MessageSendService messageSendService;
 
-    private List<Integer> lowHighRuleInstrumentConfigIds = Arrays.asList(1, 2, 4, 5, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 42, 43);
+    private List<Integer> lowHighRuleInstrumentConfigIds = Arrays.asList(1, 2, 4, 5, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 42, 43,45);
 
     private List<Integer> mainsInstrumentConfigIds = Arrays.asList(10);
 
