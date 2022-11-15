39b821e777cef1ce071afd71d69751e559f68a42
liu <111111>
2022-11-15 11:00:19 +0800

    sys-1-(xiugai mswk id)-liuzhihao

--- hc-labredis-ms/src/main/java/com/hc/HcLabredisMsApplication.java
+++ hc-labredis-ms/src/main/java/com/hc/HcLabredisMsApplication.java
@@ -1,13 +1,11 @@
 package com.hc;
 
-import com.hc.web.authorization.EnableAuthorizationX;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableAuthorizationX
 public class HcLabredisMsApplication {
 
     public static void main(String[] args) {
