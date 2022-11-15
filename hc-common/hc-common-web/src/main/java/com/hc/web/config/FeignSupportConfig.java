04da360631d75d649911664a266b77ff39a1b240
liu <111111>
2022-11-15 10:56:40 +0800

    sys-1-(xiugai mswk id)-liuzhihao

--- hc-common/hc-common-web/src/main/java/com/hc/web/config/FeignSupportConfig.java
+++ hc-common/hc-common-web/src/main/java/com/hc/web/config/FeignSupportConfig.java
@@ -1,20 +1,20 @@
-package com.hc.web.config;
-
-import com.hc.web.handler.FeignBasicAuthRequestInterceptor;
-import feign.RequestInterceptor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-/**
- * @author LiuZhiHao
- * @date 2019/10/18 9:36
- * 描述:
- **/
-@Configuration
-public class FeignSupportConfig {
-
-    @Bean
-    public RequestInterceptor requestInterceptor(){
-        return new FeignBasicAuthRequestInterceptor();
-    }
-}
+//package com.hc.web.config;
+//
+//import com.hc.web.handler.FeignBasicAuthRequestInterceptor;
+//import feign.RequestInterceptor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * @author LiuZhiHao
+// * @date 2019/10/18 9:36
+// * 描述:
+// **/
+//@Configuration
+//public class FeignSupportConfig {
+//
+//    @Bean
+//    public RequestInterceptor requestInterceptor(){
+//        return new FeignBasicAuthRequestInterceptor();
+//    }
+//}
