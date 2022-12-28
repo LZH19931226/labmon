907a1e31ece190c98e159fc8d45cf6a53b5b8364
Administrator <327909552@qq.com>
2022-12-28 17:12:08 +0800

    HC-添加ApiOperation信息-ck

--- hc-labmon-ms/src/main/java/com/hc/controller/StatisticalAnalysisController.java
+++ hc-labmon-ms/src/main/java/com/hc/controller/StatisticalAnalysisController.java
@@ -10,6 +10,7 @@ import com.hc.application.response.SummaryOfAlarmsResult;
 import com.hc.clickhouse.po.Monitorequipmentlastdata;
 import com.hc.dto.CurveInfoDto;
 import com.hc.my.common.core.jwt.JwtIgnore;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,6 +30,7 @@ public class StatisticalAnalysisController {
      * @return
      */
     @GetMapping("/getStatisticalData")
+    @ApiOperation("获取统计数据")
     public Map<String, Map<String,Long>> getStatisticalData(@RequestParam("time") String time){
         return statisticalAnalysisApplication.getStatisticalData(time);
     }
@@ -40,6 +42,7 @@ public class StatisticalAnalysisController {
      */
     @JwtIgnore
     @PostMapping("/getEquipmentData")
+    @ApiOperation("分页查询")
     public Page<Monitorequipmentlastdata> getEquipmentData(@RequestBody EquipmentDataCommand equipmentDataCommand){
         return statisticalAnalysisApplication.getEquipmentData(equipmentDataCommand);
     }
@@ -52,6 +55,7 @@ public class StatisticalAnalysisController {
      */
     @JwtIgnore
     @GetMapping("/exportEquipmentData")
+    @ApiOperation("导出设备数据")
     public void exportEquipmentData(EquipmentDataCommand equipmentDataCommand,HttpServletResponse response){
         statisticalAnalysisApplication.exportEquipmentData(equipmentDataCommand,response);
     }
@@ -62,6 +66,7 @@ public class StatisticalAnalysisController {
      */
     @JwtIgnore
     @PostMapping("/getSummaryOfAlarms")
+    @ApiOperation("查询报警汇总图")
     public SummaryOfAlarmsResult getSummaryOfAlarms(@RequestBody EquipmentDataCommand equipmentDataCommand){
         return statisticalAnalysisApplication.getSummaryOfAlarms(equipmentDataCommand);
     }
@@ -70,6 +75,7 @@ public class StatisticalAnalysisController {
      * 时间点查询 接口3,曲线
      */
     @PostMapping("/getThePointInTimeDataCurve")
+    @ApiOperation("获取时间点曲线")
     public Map<String,CurveInfoDto> getThePointInTimeDataCurve(@RequestBody EquipmentDataCommand equipmentDataCommand){
         return statisticalAnalysisApplication.getThePointInTimeDataCurve(equipmentDataCommand);
     }
@@ -78,6 +84,7 @@ public class StatisticalAnalysisController {
      * 时间点查询 接口4：表格
      */
     @PostMapping("/getThePointInTimeDataTable")
+    @ApiOperation("获取时间点数据表")
     public   List<PointInTimeDataTableResult> getThePointInTimeDataTable(@RequestBody EquipmentDataCommand equipmentDataCommand){
        return statisticalAnalysisApplication.getThePointInTimeDataTable(equipmentDataCommand);
     }
@@ -87,6 +94,7 @@ public class StatisticalAnalysisController {
      */
     @JwtIgnore
     @GetMapping("/exportDatePoint")
+    @ApiOperation("导出excel")
     public void exportDatePoint(EquipmentDataCommand equipmentDataCommand,HttpServletResponse httpServletResponse){
          statisticalAnalysisApplication.exportDatePoint(equipmentDataCommand,httpServletResponse);
     }
@@ -100,6 +108,7 @@ public class StatisticalAnalysisController {
      * @return
      */
     @PostMapping("/getAlarmNotice")
+    @ApiOperation("获取报警数据")
     public Page getAlarmNotice(@RequestBody AlarmNoticeCommand alarmNoticeCommand){
         return statisticalAnalysisApplication.getAlarmNotice(alarmNoticeCommand);
     }
@@ -110,6 +119,7 @@ public class StatisticalAnalysisController {
      */
     @JwtIgnore
     @GetMapping("/exportAlarmNotice")
+    @ApiOperation("导出报警数据")
     public void exportAlarmNotice(AlarmNoticeCommand alarmNoticeCommand, HttpServletResponse response){
         statisticalAnalysisApplication.exportAlarmNotice(alarmNoticeCommand,response);
     }
