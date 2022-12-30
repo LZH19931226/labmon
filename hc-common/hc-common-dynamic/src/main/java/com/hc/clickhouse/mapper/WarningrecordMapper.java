c95b6f4af91b1e1c6b33483b90d6b3191ff0a9ba
Administrator <327909552@qq.com>
2022-12-30 11:41:17 +0800

    HC-修改clickhouse的xml文件-ck

--- hc-common/hc-common-dynamic/src/main/java/com/hc/clickhouse/mapper/WarningrecordMapper.java
+++ hc-common/hc-common-dynamic/src/main/java/com/hc/clickhouse/mapper/WarningrecordMapper.java
@@ -20,24 +20,18 @@ public interface WarningrecordMapper extends BaseMapper<Warningrecord> {
 
     List<Warningrecord> getWarningInfoList(@Param("hospitalCode") String hospitalCode,
                                            @Param("ymd") String ymd);
-    @Select("select hospitalcode,equipmentno,inputdatetime from lab_mon.warningrecord where hospitalcode = #{hospitalCode} and formatDateTime(inputdatetime ,'%Y-%m-%d %H:%M:%S') BETWEEN #{startTime} and #{endTime}")
+
     List<Warningrecord> getWarningEquuipmentInfos(@Param("hospitalCode") String hospitalCode,
                                        @Param("startTime") String startTime,
                                        @Param("endTime") String endTime);
 
-    @Select("select * from lab_mon.warningrecord where equipmentno = #{equipmentNo} and formatDateTime(inputdatetime ,'%Y-%m-%d %H:%M:%S') BETWEEN #{startTime} and #{endTime} order by inputdatetime desc")
+
     List<Warningrecord> getWarningRecordDetailInfo(Page page,
             @Param("equipmentNo") String equipmentNo,
                                                    @Param("startTime") String startTime,
                                                    @Param("endTime") String endTime);
 
-    @Select("select " +
-            "hospitalcode, " +
-            "equipmentno, " +
-            "COUNT(instrumentparamconfigno) num " +
-            "FROM " +
-            "lab_mon.warningrecord " +
-            "WHERE toYYYYMM ( inputdatetime ) IN ( #{time} ) GROUP BY hospitalcode ,equipmentno  order BY  hospitalcode ")
+
     List<Warningrecord> getWarningInfoByTime(@Param("time") String time);
 
     List<Warningrecord> getSummaryOfAlarms(@Param("param") EquipmentDataParam convert);
