11336fa8582184abd849eab1db6b437d45f2a37d
Administrator <327909552@qq.com>
2022-12-30 11:01:13 +0800

    HC-修改曲线-ck

--- hc-labmon-ms/src/main/java/com/hc/util/CurveUtils.java
+++ hc-labmon-ms/src/main/java/com/hc/util/CurveUtils.java
@@ -8,6 +8,8 @@ import com.hc.dto.CurveInfoDto;
 import com.hc.dto.InstrumentParamConfigDto;
 import com.hc.my.common.core.constant.enums.DataFieldEnum;
 import com.hc.my.common.core.util.ObjectConvertUtils;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.apache.commons.collections4.CollectionUtils;
 
 import java.util.*;
@@ -15,31 +17,199 @@ import java.util.*;
 public class CurveUtils {
 
     public static CurveInfoDto getCurveFirst(List<Monitorequipmentlastdata> lastDataModelList, List<String> instrumentConfigIdList, Map<String, List<InstrumentParamConfigDto>> map) {
-        Map<String, List<String>> containerMap = getList(instrumentConfigIdList);
-        String time = "inputdatetime";
-        instrumentConfigIdList.add(time);
+        Map<String, Curve> containerMap = getList(instrumentConfigIdList);
         for (Monitorequipmentlastdata monitorequipmentlastdata : lastDataModelList) {
             Map<String, Object> objectToMap = ObjectConvertUtils.getObjectToMap(monitorequipmentlastdata);
             filterMap(objectToMap,instrumentConfigIdList);
             for (String field : instrumentConfigIdList) {
-                String str =  (String)objectToMap.get(field);
-                if(StringUtils.isBlank(str)){
+                if(!containerMap.containsKey(field) || !objectToMap.containsKey(field) || StringUtils.isBlank((String)objectToMap.get(field))){
                     continue;
                 }
-                List<String> stringList = containerMap.get(field);
-                stringList.add(str);
+                String str =  (String)objectToMap.get(field);
+                Curve curve = containerMap.get(field);
+                List<String> dataList = curve.getDataList();
+                dataList.add(str);
+                String timeStr =  (String)objectToMap.get("inputdatetime");
+                curve.getDateList().add(timeStr);
             }
         }
-        instrumentConfigIdList.remove(time);
         CurveInfoDto curveInfoDto = new CurveInfoDto();
         for (String field : instrumentConfigIdList) {
-            List<String> valueList = containerMap.get(field);
-            List<String> timeList = containerMap.get(time);
-            String lastDataField = DataFieldEnum.fromByLastDataField(field).getLastDataField();
-            CurveDataModel curveDataModel = generateCurveDataModel(valueList, timeList, map.get(lastDataField));
-            setCurveInfo(curveInfoDto,curveDataModel,field);
+            if(containerMap.containsKey(field)){
+                Curve curve = containerMap.get(field);
+                List<String> dataList = curve.getDataList();
+                List<String> dateList = curve.getDateList();
+                String lastDataField = DataFieldEnum.fromByLastDataField(field).getLastDataField();
+                CurveDataModel curveDataModel = generateCurveDataModel(dataList, dateList, map.get(lastDataField));
+                setCurveInfo(curveInfoDto,curveDataModel,field);
+            }
         }
         return curveInfoDto;
+    }
+
+    private static  CurveDataModel generateCurveDataModel(List<String> dataList, List<String> timeList,List<InstrumentParamConfigDto> list){
+        CurveDataModel curveDataModel = new CurveDataModel();
+        curveDataModel.setXaxis(timeList);
+        SeriesDataModel seriesDataModel = new SeriesDataModel();
+        seriesDataModel.setDate(dataList);
+        if (CollectionUtils.isNotEmpty(list)) {
+            curveDataModel.setMaxNum(list.get(0).getHighLimit()+"");
+            curveDataModel.setMinNum(list.get(0).getLowLimit()+"");
+            curveDataModel.setStyleMin(list.get(0).getStyleMin()+"");
+            curveDataModel.setStyleMax(list.get(0).getStyleMax()+"");
+        }else {
+            OptionalDouble max = dataList.stream().mapToDouble(Double::parseDouble).max();
+            if (max.isPresent()) {
+                curveDataModel.setMaxNum(max.getAsDouble()+"");
+                curveDataModel.setStyleMax(max.getAsDouble()+"");
+            }else {
+                curveDataModel.setMaxNum("");
+                curveDataModel.setStyleMax("");
+            }
+            OptionalDouble min = dataList.stream().mapToDouble(Double::parseDouble).min();
+            if (min.isPresent()) {
+                curveDataModel.setMinNum(min.getAsDouble()+"");
+                curveDataModel.setStyleMin(min.getAsDouble()+"");
+            }else {
+                curveDataModel.setMinNum("");
+                curveDataModel.setStyleMin("");
+            }
+        }
+        List<SeriesDataModel> seriesDataModelList = new ArrayList<SeriesDataModel>();
+        seriesDataModelList.add(seriesDataModel);
+        curveDataModel.setSeries(seriesDataModelList);
+        return  curveDataModel;
+    }
+
+    private static void filterMap(Map<String, Object> objectMap,List<String> list) {
+        list.add("inputdatetime");
+        Iterator<String> iterator = objectMap.keySet().iterator();
+        while (iterator.hasNext()) {
+            String next = iterator.next();
+            if(!list.contains(next)){
+                iterator.remove();
+                objectMap.remove(next);
+            }
+        }
+        list.remove("inputdatetime");
+    }
+
+    public static Map<String,Curve> getList(List<String> list){
+        Map<String,Curve> map = new HashMap<>();
+        for (String field : list) {
+            Curve curve = new Curve();
+            switch (field) {
+                case "currenttemperature":
+                    map.put("currenttemperature",curve);
+                    break;
+                case "currentcarbondioxide":
+                    map.put("currentcarbondioxide",curve);
+                    break;
+                case "currento2":
+                    map.put("currento2",curve);
+                    break;
+                case "currentairflow":
+                    map.put("currentairflow",curve);
+                    break;
+                case "currenthumidity":
+                    map.put("currenthumidity",curve);
+                    break;
+                case "currentvoc":
+                    map.put("currentvoc",curve);
+                    break;
+                case "currentformaldehyde":
+                    map.put("currentformaldehyde",curve);
+                    break;
+                case "currentpm25":
+                    map.put("currentpm25",curve);
+                    break;
+                case "currentpm10":
+                    map.put("currentpm10",curve);
+                    break;
+                case "currentqc":
+                    map.put("currentqc",curve);
+                    break;
+                case "currentairflow1":
+                    map.put("currentairflow1",curve);
+                    break;
+                case "currenttemperature1":
+                    map.put("currenttemperature1",curve);
+                    break;
+                case "currenttemperature2":
+                    map.put("currenttemperature2",curve);
+                    break;
+                case "currenttemperature3":
+                    map.put("currenttemperature3",curve);
+                    break;
+                case "currenttemperature4":
+                    map.put("currenttemperature4",curve);
+                    break;
+                case "currenttemperature5":
+                    map.put("currenttemperature5",curve);
+                    break;
+                case "currenttemperature6":
+                    map.put("currenttemperature6",curve);
+                    break;
+                case "currenttemperature7":
+                    map.put("currenttemperature7",curve);
+                    break;
+                case "currenttemperature8":
+                    map.put("currenttemperature8",curve);
+                    break;
+                case "currenttemperature9":
+                    map.put("currenttemperature9",curve);
+                    break;
+                case "currenttemperature10":
+                    map.put("currenttemperature10",curve);
+                    break;
+                case "currentlefttemperature":
+                    map.put("currentlefttemperature",curve);
+                    break;
+                case "currentrigthtemperature":
+                    map.put("currentrigthtemperature",curve);
+                    break;
+                case "currentpm5":
+                    map.put("currentpm5",curve);
+                    break;
+                case "currentpm05":
+                    map.put("currentpm05",curve);
+                    break;
+                case "currentleftcovertemperature":
+                    map.put("currentleftcovertemperature",curve);
+                    break;
+                case "currentleftendtemperature":
+                    map.put("currentleftendtemperature",curve);
+                    break;
+                case "currentleftairflow":
+                    map.put("currentleftairflow",curve);
+                    break;
+                case "currentrightcovertemperature":
+                    map.put("currentrightcovertemperature",curve);
+                    break;
+                case "currentrightendtemperature":
+                    map.put("currentrightendtemperature",curve);
+                    break;
+                case "currentrightairflow":
+                    map.put("currentrightairflow",curve);
+                    break;
+                case "currentn2":
+                    map.put("currentn2",curve);
+                    break;
+                case "leftCompartmentHumidity":
+                    map.put("leftCompartmentHumidity",curve);
+                    break;
+                case "rightCompartmentHumidity":
+                    map.put("rightCompartmentHumidity",curve);
+                    break;
+                case "liquidLevel":
+                    map.put("liquidLevel",curve);
+                    break;
+                default:
+                    break;
+            }
+        }
+        map.put("inputdatetime",new Curve());
+        return map;
     }
 
     /**
@@ -153,167 +323,14 @@ public class CurveUtils {
                 break;
         }
     }
+    
+    
+    @Data
+    @NoArgsConstructor
+    public static class Curve{
 
-    private static void filterMap(Map<String, Object> objectMap,List<String> list) {
-        Iterator<String> iterator = objectMap.keySet().iterator();
-        while (iterator.hasNext()) {
-            String next = iterator.next();
-            if(!list.contains(next)){
-                iterator.remove();
-                objectMap.remove(next);
-            }
-        }
-    }
+        private List<String> dataList = new ArrayList<>();
 
-    public static Map<String,List<String>> getList( List<String> list){
-        Map<String,List<String>> map = new HashMap<>();
-        for (String field : list) {
-            List<String> valueList =  new ArrayList<>();
-            switch (field) {
-                case "currenttemperature":
-                    map.put("currenttemperature",valueList);
-                    break;
-                case "currentcarbondioxide":
-                    map.put("currentcarbondioxide",valueList);
-                    break;
-                case "currento2":
-                    map.put("currento2",valueList);
-                    break;
-                case "currentairflow":
-                    map.put("currentairflow",valueList);
-                    break;
-                case "currenthumidity":
-                    map.put("currenthumidity",valueList);
-                    break;
-                case "currentvoc":
-                    map.put("currentvoc",valueList);
-                    break;
-                case "currentformaldehyde":
-                    map.put("currentformaldehyde",valueList);
-                    break;
-                case "currentpm25":
-                    map.put("currentpm25",valueList);
-                    break;
-                case "currentpm10":
-                    map.put("currentpm10",valueList);
-                    break;
-                case "currentqc":
-                    map.put("currentqc",valueList);
-                    break;
-                case "currentairflow1":
-                    map.put("currentairflow1",valueList);
-                    break;
-                case "currenttemperature1":
-                    map.put("currenttemperature1",valueList);
-                    break;
-                case "currenttemperature2":
-                    map.put("currenttemperature2",valueList);
-                    break;
-                case "currenttemperature3":
-                    map.put("currenttemperature3",valueList);
-                    break;
-                case "currenttemperature4":
-                    map.put("currenttemperature4",valueList);
-                    break;
-                case "currenttemperature5":
-                    map.put("currenttemperature5",valueList);
-                    break;
-                case "currenttemperature6":
-                    map.put("currenttemperature6",valueList);
-                    break;
-                case "currenttemperature7":
-                    map.put("currenttemperature7",valueList);
-                    break;
-                case "currenttemperature8":
-                    map.put("currenttemperature8",valueList);
-                    break;
-                case "currenttemperature9":
-                    map.put("currenttemperature9",valueList);
-                    break;
-                case "currenttemperature10":
-                    map.put("currenttemperature10",valueList);
-                    break;
-                case "currentlefttemperature":
-                    map.put("currentlefttemperature",valueList);
-                    break;
-                case "currentrigthtemperature":
-                    map.put("currentrigthtemperature",valueList);
-                    break;
-                case "currentpm5":
-                    map.put("currentpm5",valueList);
-                    break;
-                case "currentpm05":
-                    map.put("currentpm05",valueList);
-                    break;
-                case "currentleftcovertemperature":
-                    map.put("currentleftcovertemperature",valueList);
-                    break;
-                case "currentleftendtemperature":
-                    map.put("currentleftendtemperature",valueList);
-                    break;
-                case "currentleftairflow":
-                    map.put("currentleftairflow",valueList);
-                    break;
-                case "currentrightcovertemperature":
-                    map.put("currentrightcovertemperature",valueList);
-                    break;
-                case "currentrightendtemperature":
-                    map.put("currentrightendtemperature",valueList);
-                    break;
-                case "currentrightairflow":
-                    map.put("currentrightairflow",valueList);
-                    break;
-                case "currentn2":
-                    map.put("currentn2",valueList);
-                    break;
-                case "leftCompartmentHumidity":
-                    map.put("leftCompartmentHumidity",valueList);
-                    break;
-                case "rightCompartmentHumidity":
-                    map.put("rightCompartmentHumidity",valueList);
-                    break;
-                case "liquidLevel":
-                    map.put("liquidLevel",valueList);
-                    break;
-                default:
-                    break;
-            }
-        }
-        map.put("inputdatetime",new ArrayList<>());
-        return map;
-    }
-
-    private static  CurveDataModel generateCurveDataModel(List<String> dataList, List<String> timeList,List<InstrumentParamConfigDto> list){
-        CurveDataModel curveDataModel = new CurveDataModel();
-        curveDataModel.setXaxis(timeList);
-        SeriesDataModel seriesDataModel = new SeriesDataModel();
-        seriesDataModel.setDate(dataList);
-        if (CollectionUtils.isNotEmpty(list)) {
-            curveDataModel.setMaxNum(list.get(0).getHighLimit()+"");
-            curveDataModel.setMinNum(list.get(0).getLowLimit()+"");
-            curveDataModel.setStyleMin(list.get(0).getStyleMin()+"");
-            curveDataModel.setStyleMax(list.get(0).getStyleMax()+"");
-        }else {
-            OptionalDouble max = dataList.stream().mapToDouble(Double::parseDouble).max();
-            if (max.isPresent()) {
-                curveDataModel.setMaxNum(max.getAsDouble()+"");
-                curveDataModel.setStyleMax(max.getAsDouble()+"");
-            }else {
-                curveDataModel.setMaxNum("");
-                curveDataModel.setStyleMax("");
-            }
-            OptionalDouble min = dataList.stream().mapToDouble(Double::parseDouble).min();
-            if (min.isPresent()) {
-                curveDataModel.setMinNum(min.getAsDouble()+"");
-                curveDataModel.setStyleMin(min.getAsDouble()+"");
-            }else {
-                curveDataModel.setMinNum("");
-                curveDataModel.setStyleMin("");
-            }
-        }
-        List<SeriesDataModel> seriesDataModelList = new ArrayList<SeriesDataModel>();
-        seriesDataModelList.add(seriesDataModel);
-        curveDataModel.setSeries(seriesDataModelList);
-        return  curveDataModel;
+        private List<String> dateList = new ArrayList<>();
     }
 }
