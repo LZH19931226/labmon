48d9ec4cd5fa8c25d47212400e14d2e9fb59119c
1969994698@qq.com <1969994698@qq.com>
2023-02-15 10:52:12 +0800

    新增B2,B3协议

--- HC-TCP-ms/src/main/java/com/hc/controller/SocketMsgController.java
+++ HC-TCP-ms/src/main/java/com/hc/controller/SocketMsgController.java
@@ -1,5 +1,6 @@
 package com.hc.controller;
 
+import com.hc.my.common.core.redis.dto.ParamaterModel;
 import com.hc.service.MTOnlineBeanService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
 
 @RestController
@@ -17,9 +19,17 @@ public class SocketMsgController {
 	@Autowired
 	private MTOnlineBeanService mtOnlineBeanService;
 
+
+
 	@GetMapping("/sendMsg")
 	@ApiOperation("暂时只支持mt600/mt1100设备查询对应服务器  通道信息")
 	public void sendMsg(@RequestParam("sn") String sn, @RequestParam("message") String message){
 		mtOnlineBeanService.sendMsg(sn,message);
+	}
+
+	@GetMapping("/pasedata")
+	public  List<ParamaterModel>  paseData(String cmd){
+		return mtOnlineBeanService.paseData(cmd);
+
 	}
 }
