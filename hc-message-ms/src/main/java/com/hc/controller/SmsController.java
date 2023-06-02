a630c071aa2156d437fb32fd99717e071e13cefb
1969994698@qq.com <1969994698@qq.com>
2023-06-02 11:59:10 +0800

    修改message服务配置

--- hc-message-ms/src/main/java/com/hc/controller/SmsController.java
+++ hc-message-ms/src/main/java/com/hc/controller/SmsController.java
@@ -2,6 +2,7 @@ package com.hc.controller;
 
 import com.hc.util.AmazonConnectUtil;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class SmsController {
 
 
+	@Autowired
+	private AmazonConnectUtil amazonConnectUtil;
 
 	@GetMapping("/phone")
 	@ApiOperation("电话号码")
 	public void testPhone(String phone,String message){
-		AmazonConnectUtil.makeCall(phone,message);
+		amazonConnectUtil.makeCall(phone,message);
 	}
 }
