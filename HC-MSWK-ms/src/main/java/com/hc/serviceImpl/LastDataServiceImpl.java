7a52f33dcd6cb90c0c48cb668c28e34166d06ca4
1969994698@qq.com <1969994698@qq.com>
2025-02-27 16:38:37 +0800

    全球时间时区  数据存储4

--- HC-MSWK-ms/src/main/java/com/hc/serviceImpl/LastDataServiceImpl.java
+++ HC-MSWK-ms/src/main/java/com/hc/serviceImpl/LastDataServiceImpl.java
@@ -8,12 +8,14 @@ import com.hc.device.SnDeviceRedisApi;
 import com.hc.my.common.core.redis.dto.HarvesterDto;
 import com.hc.my.common.core.redis.dto.MonitorequipmentlastdataDto;
 import com.hc.my.common.core.util.BeanConverter;
+import com.hc.my.common.core.util.DateUtils;
 import com.hc.service.LastDataService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Created by 16956 on 2018-09-04.
@@ -57,5 +59,27 @@ public class LastDataServiceImpl implements LastDataService {
         HarvesterDto harvesterDto = JSONUtil.toBean(messageContent, HarvesterDto.class);
         Harvester convert = BeanConverter.convert(harvesterDto, Harvester.class);
         harvesterRepository.save(convert);
+    }
+
+
+    public static void main(String[] args){
+//        LocalDateTime serverTime = LocalDateTime.now();
+//        // 将服务器时间转换为系统默认时区的 ZonedDateTime
+//        ZonedDateTime serverZonedDateTime = serverTime.atZone(ZoneId.systemDefault());
+//        // 将其转换为 ClickHouse 时区的 ZonedDateTime
+//        ZonedDateTime clickhouseZonedDateTime = serverZonedDateTime.withZoneSameInstant(CLICKHOUSE_ZONE);
+//        // 转换为 ClickHouse 时区的 LocalDateTime
+//        LocalDateTime clickhouseLocalDateTime = clickhouseZonedDateTime.toLocalDateTime();
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+//        LocalDateTime utcTime = LocalDateTime.now(Clock.systemUTC());
+//        ZonedDateTime phoenixTime = utcTime.atZone(ZoneOffset.UTC)
+//                .withZoneSameInstant(ZoneId.of("America/Phoenix"));
+//        LocalDateTime clickhouseLocalDateTime = phoenixTime.toLocalDateTime();
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+//        System.out.println(clickhouseLocalDateTime.format(formatter));
+        LocalDateTime serverTime = LocalDateTime.now();
+        ZonedDateTime zonedDateTime = ZonedDateTime.of(serverTime, CLICKHOUSE_ZONE);
+
+        System.out.println("带时区的时间: " + zonedDateTime);
     }
 }
